51a460b5440d5c2b679b6dcfeb735160bde56a5e
ravichandranb <asbrc2005@gmail.com>
2022-04-30 18:08:37 +0400

    Updates

---
+++ precache-manifest.27800f4df5341d4fb73b93cd14112e3c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b799dbae85a81699e99",
+    "url": "/react-deploy/static/js/main.2b799dba.chunk.js"
+  },
+  {
+    "revision": "fd6ad57b32deca1b48a3",
+    "url": "/react-deploy/static/js/1.fd6ad57b.chunk.js"
+  },
+  {
+    "revision": "00539649abe3b4db1174",
+    "url": "/react-deploy/static/js/runtime~main.00539649.js"
+  },
+  {
+    "revision": "7f4dcd351a073fc0218a4ffc1b548a57",
+    "url": "/react-deploy/index.html"
+  }
+];
